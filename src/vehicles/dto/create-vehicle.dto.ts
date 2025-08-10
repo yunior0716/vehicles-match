8b5d5de6a2cea72b161065e1f71158c7b41b32b1
Delio Yunior <yunior0716@gmail.com>
2025-08-10 01:46:53 -0400

    Added vehicles Entity and DTO

---
+++ src/vehicles/dto/create-vehicle.dto.ts
@@ -0,0 +1,55 @@
+import {
+  IsIn,
+  IsInt,
+  IsPositive,
+  IsString,
+  Min,
+  MinLength,
+} from 'class-validator';
+
+export class CreateVehicleDto {
+  @IsString()
+  @MinLength(1)
+  brand: string;
+
+  @IsString()
+  @MinLength(1)
+  model: string;
+
+  @IsInt()
+  @Min(1886) // The first car was invented in 1886
+  @IsPositive()
+  year: number;
+
+  @IsIn(['gas', 'diesel', 'hybrid', 'gasoline', 'electric'])
+  fuel: string;
+
+  @IsIn(['manual', 'automatic'])
+  transmission: string;
+
+  @IsInt()
+  @IsPositive()
+  @MinLength(1)
+  seats: number;
+
+  @IsInt()
+  @IsPositive()
+  @MinLength(1)
+  doors: number;
+
+  @IsString()
+  @MinLength(1)
+  carrocery: string;
+
+  @IsInt()
+  @IsPositive()
+  price: number;
+
+  @IsString()
+  @MinLength(1)
+  image: string;
+
+  @IsString()
+  @MinLength(1)
+  description: string;
+}
---
+++ src/vehicles/entities/vehicle.entity.ts
@@ -0,0 +1,40 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'vehicles' })
+export class Vehicle {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'text' })
+  brand: string;
+
+  @Column({ type: 'text' })
+  model: string;
+
+  @Column({ type: 'int' })
+  year: number;
+
+  @Column({ type: 'text' })
+  fuel: string;
+
+  @Column({ type: 'text' })
+  transmission: string;
+
+  @Column({ type: 'int' })
+  seats: number;
+
+  @Column({ type: 'int' })
+  doors: number;
+
+  @Column({ type: 'text' })
+  carrocery: string;
+
+  @Column({ type: 'int' })
+  price: number;
+
+  @Column({ type: 'text' })
+  image: string;
+
+  @Column({ type: 'text' })
+  description: string;
+}
